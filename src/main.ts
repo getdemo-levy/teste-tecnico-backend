b60f3b9a48cb8b1cef1dcb09a64bb33475408976
Levy Moreira Cruz <levymcruz@gmail.com>
2025-03-09 18:25:14 -0300

    fix: atualizando o cors e adicionando headers autorizados

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: ['https://main.d288zmy9qlsxi0.amplifyapp.com', 'http://localhost:3000'],
-    methods: '*',
-    credentials: true
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204
   });
   await app.listen(process.env.PORT ?? 3005);
 }
