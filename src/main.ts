7c38cdc2ae58886429c2457d47827c46f6febb91
Levy Moreira Cruz <levymcruz@gmail.com>
2025-03-09 18:19:34 -0300

    fix: removendo o delpoy.yml e corrigindo as configs do cors

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,9 @@ import { AppModule } from './modules/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://main.d288zmy9qlsxi0.amplifyapp.com/',
+    origin: ['https://main.d288zmy9qlsxi0.amplifyapp.com', 'http://localhost:3000'],
     methods: '*',
+    credentials: true
   });
   await app.listen(process.env.PORT ?? 3005);
 }
