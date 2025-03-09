b6caf1c45f9874be401e3e5047c6b17f6a087e0e
Levy Moreira Cruz <levymcruz@gmail.com>
2025-03-09 17:29:43 -0300

    fix: alterando as configurações do cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './modules/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors()
+  app.enableCors({
+    origin: 'https://main.d288zmy9qlsxi0.amplifyapp.com/',
+    methods: '*',
+  });
   await app.listen(process.env.PORT ?? 3005);
 }
 bootstrap();
